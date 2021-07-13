9109342e0421c18f1e0f4bb606acdc2b797bb7df
Sarah Wang <sarah.wang2009@gmail.com>
2021-07-13 11:41:23 -0400

    user log in, log out, registration, persisted user login

--- Sprout/View Controllers/LoginViewController.m
+++ Sprout/View Controllers/LoginViewController.m
@@ -6,8 +6,16 @@
 //
 
 #import "LoginViewController.h"
+#import "Parse/Parse.h"
 
 @interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (strong, nonatomic) UIAlertController *blankAlert;
+@property (strong, nonatomic) UIAlertController *registrationAlert;
+@property (strong, nonatomic) UIAlertController *loginAlert;
 
 @end
 
@@ -15,7 +23,83 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.blankAlert = [UIAlertController alertControllerWithTitle:@"Username or password is empty." message:@"Please try again!" preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        // handle response here.
+    }];
+    [self.blankAlert addAction:okAction];
+    
+    self.registrationAlert = [UIAlertController alertControllerWithTitle:@"Error registering user." message:@"Please try again!" preferredStyle:(UIAlertControllerStyleAlert)];
+    [self.registrationAlert addAction:okAction];
+    
+    self.loginAlert = [UIAlertController alertControllerWithTitle:@"Error during user login." message:@"Please try again!" preferredStyle:(UIAlertControllerStyleAlert)];
+    [self.loginAlert addAction:okAction];
+
+}
+
+- (IBAction)didTap:(id)sender {
+    [self.view endEditing:true];
+}
+
+- (IBAction)registerUser:(id)sender {
+    [self.activityIndicator startAnimating];
+    
+    if([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""])
+    {
+        [self presentViewController:self.blankAlert animated:YES completion:^{
+            [self.activityIndicator stopAnimating];
+        }];
+    }
+    else
+    {
+        PFUser *newUser = [PFUser user];
+        
+        newUser.username = self.usernameField.text;
+        newUser.password = self.passwordField.text;
+        
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+            if (error != nil) {
+                NSLog(@"Error: %@", error.localizedDescription);
+                [self presentViewController:self.registrationAlert animated:YES completion:^{
+                    [self.activityIndicator stopAnimating];
+                }];
+            } else {
+                NSLog(@"User registered successfully");
+                [self.activityIndicator stopAnimating];
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            }
+        }];
+    }
+}
+
+- (IBAction)loginUser:(id)sender {
+    [self.activityIndicator startAnimating];
+    
+    if([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""])
+    {
+        [self presentViewController:self.blankAlert animated:YES completion:^{
+            [self.activityIndicator stopAnimating];
+        }];
+    }
+    else
+    {
+        NSString *username = self.usernameField.text;
+        NSString *password = self.passwordField.text;
+        
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+            if (error != nil) {
+                NSLog(@"User log in failed: %@", error.localizedDescription);
+                [self presentViewController:self.loginAlert animated:YES completion:^{
+                    [self.activityIndicator stopAnimating];
+                }];
+            } else {
+                NSLog(@"User logged in successfully");
+                [self.activityIndicator stopAnimating];
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            }
+        }];
+    }
 }
 
 /*
---
+++ Sprout/View Controllers/ProfileViewController.m
@@ -0,0 +1,44 @@
+//
+//  ProfileViewController.m
+//  Sprout
+//
+//  Created by Sarah Wang on 7/13/21.
+//
+
+#import "ProfileViewController.h"
+#import "Parse/Parse.h"
+#import "LoginViewController.h"
+#import "AppDelegate.h"
+#import "SceneDelegate.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapLogout:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error)  {
+    }];
+    SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *controller = [storyboard  instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    sceneDelegate.window.rootViewController = controller;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
