67a6416b888e3297fb505114c8da0852dc2056f1
Sarah Wang <sarah.wang2009@gmail.com>
2021-07-26 15:28:26 -0400

    code fixes

--- Sprout/View Controllers/PostDetailsViewController.m
+++ Sprout/View Controllers/PostDetailsViewController.m
@@ -32,19 +32,19 @@
 }
 
 - (void)refreshData {
-    PFUser *postUser = self.post.author;
-    PFFileObject *photo = postUser[@"profileImage"];
+    PFUser *author = self.post.author;
+    PFFileObject *photo = author[@"profileImage"];
     [photo getDataInBackgroundWithBlock:^(NSData * _Nullable imageData, NSError * _Nullable error) {
         self.profileImageView.image = [UIImage imageWithData:imageData];
     }];
     self.timestampLabel.text = self.post.createdAt.shortTimeAgoSinceNow;
-    if([postUser[@"name"] isEqual:nil]){
+    if ([author[@"name"] isEqual:nil]) {
         self.usernameLabel.text = nil;
-        self.nameLabel.text = postUser.username;
+        self.nameLabel.text = author.username;
     }
-    else{
-        self.usernameLabel.text = postUser.username;
-        self.nameLabel.text = postUser[@"name"];
+    else {
+        self.usernameLabel.text = author.username;
+        self.nameLabel.text = author[@"name"];
     }
     self.progressLabel.text = [NSString stringWithFormat:@"I completed %@ of %@ tasks today!", self.post.completedTasks, self.post.totalTasks];
     self.captionLabel.text = self.post.caption;
@@ -52,8 +52,7 @@
     self.likeCountLabel.text = [NSString stringWithFormat:@"%d likes", [self.post.likeCount intValue]];
     
     NSLog(@"%@", self.post.usersWhoLiked);
-    if([self.post.usersWhoLiked containsObject:PFUser.currentUser.objectId])
-    {
+    if ([self.post.usersWhoLiked containsObject:PFUser.currentUser.objectId]) {
         [self updateLikeButton:YES];
     }
     else {
@@ -63,7 +62,7 @@
 
 - (IBAction)didTapLike:(id)sender {
     
-    if([self.post.usersWhoLiked containsObject:PFUser.currentUser.objectId])
+    if ([self.post.usersWhoLiked containsObject:PFUser.currentUser.objectId])
     {
         [self.post removeObject:PFUser.currentUser.objectId forKey:@"usersWhoLiked"];
         
@@ -86,7 +85,7 @@
 
 - (void)updateLikeButton:(BOOL) buttonStatus {
     UIColor *color = [[UIColor alloc]initWithRed:97/255.0 green:179/255.0 blue:121/255.0 alpha:1.0];
-    if(buttonStatus)
+    if (buttonStatus)
     {
         UIImage *image = [UIImage systemImageNamed:@"heart.fill" withConfiguration:[UIImageSymbolConfiguration configurationWithScale:(UIImageSymbolScaleLarge)]];
         [self.likeButton setImage:image forState:UIControlStateNormal];
